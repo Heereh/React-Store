41f0003b1416f25449befefb1bb774ce9df4b67b
Heredik <nahuel.arevalo@hotmail.com.ar>
2024-01-05 17:19:25 -0300

    Cambios en el codigo/Implementacion de varias paginas

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,48 +1,76 @@
 /* eslint-disable no-unused-vars */
 import {
-  Routes as ReactDomRoutes,
-  Route,
-  BrowserRouter,
+	Routes as ReactDomRoutes,
+	Route,
 } from 'react-router-dom';
 
 import Home from '../pages/Home/Home';
-import Products from '../layouts/Products/Products';
+import Products from '../components/Products/Products';
 import Login from '../pages/Login/Login';
 import Register from '../pages/Register/Register';
 import Contact from '../pages/Contact/Contact';
 import Product from '../pages/Product/Product';
 import UserProfiler from '../pages/User/UserProfiler';
-import { AuthProvider } from '../context/AuthContext';
+
 import ProtetedRoute from '../components/ProtetedRoute/ProtetedRoute';
-/* import AllProducts from '../pages/AllProducts/AllProducts'; */
+import ItemCart from '../pages/ItemCart/ItemCart';
+
 const Routes = () => {
-  return (
-    <AuthProvider>
-      <ReactDomRoutes>
-        <Route path="/" element={<Home />} />
+	return (
+		<ReactDomRoutes>
+			<Route
+				path="/"
+				element={<Home />}
+			/>
 
-        <Route path="products">
-          <Route index element={<Products isEnabled={true} />} />
-          <Route path=":product" element={<Product />} />
-        </Route>
+			<Route path="products">
+				<Route
+					index
+					element={<Products isEnabled={true} />}
+				/>
+				<Route
+					path=":product"
+					element={<Product />}
+				/>
+			</Route>
 
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
+			<Route
+				path="login"
+				element={<Login />}
+			/>
+			<Route
+				path="register"
+				element={<Register />}
+			/>
 
-        <Route
-          path="user/:username"
-          element={
-            <ProtetedRoute redirectTo={'/login'}>
-              <UserProfiler />
-            </ProtetedRoute>
-          }
-        />
+			<Route
+				path="user/:username"
+				element={
+					<ProtetedRoute redirectTo={'/login'}>
+						<UserProfiler />
+					</ProtetedRoute>
+				}
+			/>
 
-        <Route path="contact" element={<Contact />} />
-        <Route path="*" element={<h2>ERROR!</h2>} />
-      </ReactDomRoutes>
-    </AuthProvider>
-  );
+			<Route
+				path="cart"
+				element={
+					<ProtetedRoute redirectTo={'/login'}>
+						<ItemCart />
+					</ProtetedRoute>
+				}
+			/>
+
+			<Route
+				path="contact"
+				element={<Contact />}
+			/>
+			<Route
+				path="*"
+				element={<h2>ERROR!</h2>}
+			/>
+		</ReactDomRoutes>
+	);
 };
 
 export default Routes;
