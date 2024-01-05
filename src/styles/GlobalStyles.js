7cee05c929fd25e8b9bd7e6e3d3ef799ef088804
Heredik <nahuel.arevalo@hotmail.com.ar>
2024-01-05 16:48:35 -0300

    Estilos globales para la pagina

---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyled = createGlobalStyle`
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  list-style: none;
+  text-decoration: none;
+}
+
+hmtl{
+  
+}
+body{
+  min-height: 100%;
+  position: relative;
+ 
+}
+`;
