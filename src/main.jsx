7b526da23906360eec8c22ec8fd5d32beb5e9f41
Heredik <nahuel.arevalo@hotmail.com.ar>
2024-01-05 17:11:51 -0300

    Pequeños camci

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,21 @@ import './index.css';
 
 import { NextUIProvider } from '@nextui-org/react';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext.jsx';
+//import { AuthProvider } from './context/AuthContext.jsx';
+import { Provider } from 'react-redux';
+import { persistor, store } from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NextUIProvider>
-      <BrowserRouter>
-        <AuthProvider>
-          <App />
-        </AuthProvider>
-      </BrowserRouter>
-    </NextUIProvider>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<Provider store={store}>
+			<NextUIProvider>
+				<BrowserRouter>
+					<PersistGate persistor={persistor}>
+						<App />
+					</PersistGate>
+				</BrowserRouter>
+			</NextUIProvider>
+		</Provider>
+	</React.StrictMode>,
 );
