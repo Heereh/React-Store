c30052abed2ec7ee77c41e070c9e239e668514f6
Heredik <nahuel.arevalo@hotmail.com.ar>
2024-01-05 17:04:43 -0300

    Pequeños cambios

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,17 @@ import { nextui } from '@nextui-org/react';
 
 /** @type {import('tailwindcss').Config} */
 export const content = [
-  './index.html',
-  './src/**/*.{js,ts,jsx,tsx}',
+	'./index.html',
+	'./src/**/*.{js,ts,jsx,tsx}',
 
-  './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+	'./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
 ];
 export const theme = {
-  extend: {
-    maxWidth:{
-      '1400':1400
-    }
-  },
+	extend: {
+		maxWidth: {
+			1400: 1400,
+		},
+	},
 };
 export const darkMode = 'class';
 export const plugins = [nextui()];
