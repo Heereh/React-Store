09575f05200675d0ec0827e894145eb41670aaf3
Heredik <nahuel.arevalo@hotmail.com.ar>
2024-01-05 17:21:08 -0300

    .

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,15 +1,13 @@
 import { useParams } from 'react-router-dom';
 
 const Product = () => {
-  
-
-  const { product } = useParams();
- 
-  return (
-    <div>
-      <h2>Seccion de {product}</h2>
-    </div>
-  );
+	const { product } = useParams();
+	
+	return (
+		<div>
+			<h2>Seccion de {product}</h2>
+		</div>
+	);
 };
 
 export default Product;
